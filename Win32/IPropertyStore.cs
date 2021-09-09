5f2640a512d3a5753e3dc355f3473eea01532a59
Amer Koleci <amerkoleci@gmail.com>
2021-09-09 14:00:49 +0200

    Add support for PropertyKey (PROPERTYKEY) and IPropertyStore.

---
+++ Win32/IPropertyStore.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SharpGen.Runtime.Win32
+{
+    public partial class IPropertyStore
+    {
+        public Variant GetValue(PropertyKey key) => GetValue(ref key);
+
+        public void SetValue(PropertyKey key, Variant value) => SetValue(ref key, value);
+    }
+}
---
+++ Win32/PropertyKey.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SharpGen.Runtime.Win32
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public readonly struct PropertyKey
+    {
+        /// <summary>
+        /// Format ID
+        /// </summary>
+        public readonly Guid FormatId;
+        /// <summary>
+        /// Property ID
+        /// </summary>
+        public readonly int PropertyId;
+
+        /// <summary>
+        /// <param name="formatId"></param>
+        /// <param name="propertyId"></param>
+        /// </summary>
+        public PropertyKey(Guid formatId, int propertyId)
+        {
+            FormatId = formatId;
+            PropertyId = propertyId;
+        }
+    }
+}
